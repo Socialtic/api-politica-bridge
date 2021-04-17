36e3707d8a7bb4aa4cc94d6447a1c204fbe9a04c
Diego B <dbarriga@ciencias.unam.mx>
2021-04-17 17:03:28 -0500

    feature(auto): Person Table
    Automatic construction of Person Table

--- tester.py
+++ tester.py
@@ -1,28 +1,32 @@
-import csv
 from sheets_reader import get_sheets_data
 from validations import (last_name_check, membership_type_check,
-                        date_format_check, url_check, profession_check, url_other_check)
-from utils import (make_banner, row_to_dict, get_columns_data, write_report)
+                         date_format_check, url_check, profession_check,
+                         url_other_check)
+from utils import *
 
-SHEET_ID = "1mk9LTI5RBYwrEPzILeDY925VJbLVmEoZyRzaa1gZ_hk"
+CAPTURE_SHEET_ID = "1mk9LTI5RBYwrEPzILeDY925VJbLVmEoZyRzaa1gZ_hk"
+STRUCT_SHEET_ID = "1fKXpwXhKlLLG-kjh8udQIH9poNLs7kAzSnXndZ1Le4Y"
 # Ranges of the sheet to be read
-READ_RANGES = ["Gubernaturas!A1:AB114",]
+READ_RANGES = ["Gubernaturas!A1:AB114", "Alcaldías!A1:AC63"]
 # Only test in this columns
 TEST_FIELDS = ["last_name", "membership_type", "start_date", "end_date",
-                "date_birth", "profession_2", "profession_3",
-                "profession_4", "profession_5", "profession_6", "Website",
-                "URL_FB_page", "URL_FB_profile", "URL_IG", "URL_TW", "URL_others"]
+               "date_birth", "profession_2", "profession_3",
+               "profession_4", "profession_5", "profession_6", "Website",
+               "URL_FB_page", "URL_FB_profile", "URL_IG", "URL_TW", "URL_others"]
+CONTEST_CHAMBERS = get_sheets_data(STRUCT_SHEET_ID, "Table contest!C2:C357")
 
 def main():
     # Main loop throught sheet pages
     for read_range in READ_RANGES:
-        make_banner(f"Runing test suite for {read_range}")
+        current_chamber = read_range.split('!')[0].lower()
+        make_banner(f"Runing test suite for >> {current_chamber}")
         # Getting sheet data as list of list
-        dataset = get_sheets_data(SHEET_ID, read_range)
+        dataset = get_sheets_data(CAPTURE_SHEET_ID, read_range)
         # Ignoring header
         header = dataset.pop(0)
         # Removing start and end spaces of header items
         header = [h.strip() for h in header]
+        dict_dataset = [row_to_dict(row, header) for row in dataset]
         lines = []
         # Loop to read every row
         for i, row in enumerate(dataset, start=2):
@@ -44,16 +48,35 @@ def main():
             line += ",URL_IG" if not url_check(data["URL_IG"], "strict") else ""
             line += ",URL_TW" if not url_check(data["URL_TW"], "strict") else ""
             url_others = data["URL_others"].split(",")
-            line +=  url_other_check(url_others)
+            line += url_other_check(url_others)
             # If no errors, discard this line
             if line == str(i):
+                line = ''
                 continue
             # Adding candidate name at the end of the line
             else:
                 line += f",{row[3]}"
-            lines.append(line)
+                lines.append(line)
+
+        # Making structured data
+        # TODO: person, other-name, person-profession, membership, url se pueden llenar desde hoja de captura
+        breakpoint()
+        # Ignoring last 's' from gubernaturas
+        current_chamber = current_chamber[:-1]
+        # person table header
+        header = ["full_name", "first_name", "last_name", "date_birth", "gender",
+              "dead_or_alive", "last_degree_of_studies", "contest_id"]
+        people = make_person_struct(dict_dataset, current_chamber,
+                                    CONTEST_CHAMBERS, header)
+        person_table = make_table(header, people)
+        write_report(person_table, "person_struct")
+        other_name_header = ["other_name_type", "name", "person_id"]
+
         # Writing report
-        write_report("\n".join(lines))
+        write_report("\n".join(lines), read_range.split("!")[0])
+
+
+
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,9 +1,28 @@
-import re
+class Catalogues:
+    DEGREES_OF_STUDIES = ['', 'ELEMENTARY', 'HIGH SCHOOL', 'ASSOCIATE DEGREE',
+                          'BACHELOR’S DEGREE',
+                          'UNIVERSITY 1ST PROFESSIONAL DEGREE',
+                          'MASTER DEGREE', 'PHD DEGREE']
+    #   GOBERNADOR, DIPUTADO , PRESIDENTE MUNICIPAL
+    DISTRICT_TYPES = ['NATIONAL_EXEC', 'REGIONAL_EXEC', 'NATIONAL_LOWER', 'LOCAL_EXEC']
+    GENDERS = ['', 'M', 'F']
+    MEMBERSHIP_TYPES = ['', 'officeholder', 'campaigning_politician', 'party_leader']
+    # privilegiado, apodo, nombre de la boleta
+    OTHER_NAMES_TYPES = ['', 'preferred', 'nickname', 'ballot_name']
+    #   GOBERNADOR, DIPUTADO , PRESIDENTE MUNICIPAL
+    ROLE_TYPES = ['', 'governmentOfficer', 'legislatorLowerBody', 'executiveCouncil']
+    URL_TYPE_NAMES = ['',
+                      'campaign', 'official', 'personal', 'wikipedia',
+                      'campaign', 'official', 'personal',
+                      'campaign', 'official', 'personal',
+                      'WhatsApp', 'Twitter', 'YouTube', 'LinkedIn', 'Flickr',
+                      'Pinterest', 'Tumblr', 'RSS', ]
+
 
 def make_banner(title):
     """**Make a banner for visual purposes**
 
-    :param title: Messages to be highlighted   
+    :param title: Messages to be highlighted
     :type: str
     """
     print()
@@ -22,10 +41,10 @@ def get_columns_data(data, pattern):
     :return: Dict with key-value that match pattern
     :rtype: dict
     """
-    return {field:data[field] for field in data if pattern in field}
+    return {field: data[field] for field in data if pattern in field}
 
 
-def row_to_dict(row, header, test_fields):
+def row_to_dict(row, header, test_fields=[]):
     """**Converts from a row of the sheet to a python dict**
 
     :param row: Current row of the sheet
@@ -36,23 +55,75 @@ def row_to_dict(row, header, test_fields):
     :rtype: dict
     """
     data = dict()
+    columns = test_fields if test_fields else header
     # Making dict with column-value pairs
-    for column in test_fields:
+    for column in columns:
         try:
             data[column] = row[header.index(column)]
         # Row is shorter than header. Last fields must be empty
         except IndexError:
             data[column] = ''
-            continue 
+            continue
     return data
 
 
-def write_report(data):
+def write_report(data, name):
     """**Write a report with the errors found  at the sheet**
 
     :param data: A comma separated string with line number and fields with errors
     :type data: str
     """
-    with open("report.csv", 'w') as f:
+    with open(f"{name}_report.csv", 'w', encoding="utf-8") as f:
         f.write(data)
 
+
+def my_str_to_bool(row, endpoint):
+    """
+    docstring
+    """
+    p_fields = ["dead_or_alive"]
+    m_fields = ["goes_for_coalition", "goes_for_reelection", "is_substitute", "changed_from_sustitute"]
+    if endpoint == "person":
+        fields = p_fields
+    elif endpoint == "membership":
+        fields = m_fields
+    for field in fields:
+        row[field] = True if row[field] == "TRUE" else False
+    return row
+
+
+def get_contest_id(state, current_chamber, contest_chambers):
+    state = state.lower()
+    current_chamber = current_chamber.lower()
+    for i, contest_chamber in enumerate(contest_chambers, start=1):
+        contest_chamber = contest_chamber[0].lower()
+        if state in contest_chamber and current_chamber in contest_chamber:
+            return i
+
+
+def make_person_struct(dataset, chamber, contest_chambers, header):
+    people = []
+    for data in dataset:
+        row = dict()
+        for field in header:
+            if field == "gender":
+                row[field] = 2 if data[field] == "F" else 1
+            elif field == "dead_or_alive":
+                row[field] = True if data[field] == "Vivo" else False
+            elif field == "last_degree_of_studies":
+                row[field] = Catalogues.DEGREES_OF_STUDIES.index(data[field].upper())
+            elif field == "contest_id":
+                row[field] = get_contest_id(data["state"], chamber, contest_chambers)
+            else:
+                row[field] = data[field]
+        people.append(row)
+    return people
+
+
+def make_table(header, dataset):
+    table = ','.join(header) + '\n'
+    for row_data in dataset:
+        for field in header:
+            table += f"{row_data[field]},"
+        table += '\n'
+    return table
