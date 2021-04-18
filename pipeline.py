970bfd007acf60988789a3f210a59e2ac8803674
Diego B <dbarriga@ciencias.unam.mx>
2021-04-17 19:10:42 -0500

    feature(table): other-name
    Refactoring test suite and add data construction. This data will be
    sent to the API

---
+++ pipeline.py
@@ -0,0 +1,73 @@
+from sheets_reader import get_sheets_data
+from utils import *
+
+CAPTURE_SHEET_ID = "1mk9LTI5RBYwrEPzILeDY925VJbLVmEoZyRzaa1gZ_hk"
+STRUCT_SHEET_ID = "1fKXpwXhKlLLG-kjh8udQIH9poNLs7kAzSnXndZ1Le4Y"
+# Ranges of the sheet to be read
+READ_RANGES = ["Gubernaturas!A1:AB114", "Alcaldías!A1:AC63"]
+# Only test in this columns
+TEST_FIELDS = ["last_name", "membership_type", "start_date", "end_date",
+               "date_birth", "profession_2", "profession_3",
+               "profession_4", "profession_5", "profession_6", "Website",
+               "URL_FB_page", "URL_FB_profile", "URL_IG", "URL_TW",
+               "URL_others"]
+CONTEST_CHAMBERS = get_sheets_data(STRUCT_SHEET_ID, "Table contest!C2:C357")
+
+REPORT_PATH = "reports"
+TABLES_PATH = "structured_tables"
+
+
+def main():
+    # Main loop throught sheet pages
+    for read_range in READ_RANGES[:1]: # Testing only on Gubernaturas
+        current_chamber = read_range.split('!')[0].lower()
+        make_banner(f"Data from >> {current_chamber}")
+        # Getting sheet data as list of list
+        dataset = get_sheets_data(CAPTURE_SHEET_ID, read_range)
+        # Ignoring header
+        header = dataset.pop(0)
+        # Removing start and end spaces of header items
+        header = [h.strip() for h in header]
+        # Convert list to dict
+        dict_dataset = [row_to_dict(row, header) for row in dataset]
+        # Start capture verification
+        make_banner("Tests Suite begin")
+        error_lines = verification_process(dataset, header)
+        if error_lines:
+            # Writing report
+            write_report("\n".join(error_lines),
+                         f"{REPORT_PATH}/{current_chamber}_errors")
+        make_banner("Preprocessing Data")
+        # Making structured data
+        # TODO: Get the information of tables that are already finished
+        # TODO: area, chamber, role, coalition, party, profession, contest(?),
+        # TODO: Make this dynamic
+        # Ignoring last 's' from gubernaturas
+        current_chamber = current_chamber[:-1]
+        # Making person data
+        person_header = ["full_name", "first_name", "last_name", "date_birth",
+                         "gender", "dead_or_alive", "last_degree_of_studies",
+                         "contest_id"]
+        # This list is ready to be send to the API
+        person_data = make_person_struct(dict_dataset, current_chamber,
+                                    CONTEST_CHAMBERS, person_header)
+        # Making a table for double check
+        person_table = make_table(person_header, person_data)
+        write_report(person_table, f"{TABLES_PATH}/person")
+        # Making other-name data
+        other_name_header = ["other_name_type", "name", "person_id"]
+        # This list is ready to be send to the API
+        other_names_data = make_other_names_struct(dict_dataset)
+        # Making a table for double check
+        other_name_table = make_table(other_name_header, other_names_data)
+        write_report(other_name_table, f"{TABLES_PATH}/other-name")
+        # TODO Making person-profession data
+        professions_catalogue = get_sheets_data(STRUCT_SHEET_ID,
+                                               "Catalogue profession!B2:B120")
+        # person_profession_data = make_person_profession(dataset, professions_catalogue)
+        # TODO Making membership data
+        # TODO Making url data
+
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+from validations import (last_name_check, membership_type_check,
+                         date_format_check, url_check, profession_check,
+                         url_other_check)
+
 class Catalogues:
     DEGREES_OF_STUDIES = ['', 'ELEMENTARY', 'HIGH SCHOOL', 'ASSOCIATE DEGREE',
                           'BACHELOR’S DEGREE',
@@ -67,13 +71,48 @@ def row_to_dict(row, header, test_fields=[]):
     return data
 
 
+def verification_process(dataset, header):
+    lines = []
+    # Loop to read every row
+    for i, row in enumerate(dataset, start=2):
+        print(f"Checking row #{i} = {row[3]}")
+        line = f'{i}'
+        # Change row from list to dict with relevant information (test fields)
+        data = row_to_dict(row, header)
+        # Tests suite start
+        line += ",last_name" if not last_name_check(data["last_name"] or []) else ""
+        line += ",membership_type" if not membership_type_check(data["membership_type"]) else ""
+        line += ",start_date" if not date_format_check(data["start_date"]) else ""
+        line += ",end_date" if not date_format_check(data["end_date"]) else ""
+        line += ",date_birth" if not date_format_check(data["date_birth"]) else ""
+        professions = get_columns_data(data, "profession")
+        # TODO: from 2 to 6 only
+        line += ",professions" if not profession_check(professions.values()) else ""
+        line += ",Website" if not url_check(data["Website"], "light") else ""
+        line += ",URL_FB_page" if not url_check(data["URL_FB_page"], "strict") else ""
+        line += ",URL_FB_profile" if not url_check(data["URL_FB_profile"], "strict") else ""
+        line += ",URL_IG" if not url_check(data["URL_IG"], "strict") else ""
+        line += ",URL_TW" if not url_check(data["URL_TW"], "strict") else ""
+        url_others = data["URL_others"].split(",")
+        line += url_other_check(url_others)
+        # If no errors, discard this line
+        if line == str(i):
+            line = ''
+            continue
+        # Adding candidate name at the end of the line
+        else:
+            line += f",{row[3]}"
+            lines.append(line)
+    return lines
+
+
 def write_report(data, name):
     """**Write a report with the errors found  at the sheet**
 
     :param data: A comma separated string with line number and fields with errors
     :type data: str
     """
-    with open(f"{name}_report.csv", 'w', encoding="utf-8") as f:
+    with open(f"{name}.csv", 'w', encoding="utf-8") as f:
         f.write(data)
 
 
@@ -90,6 +129,15 @@ def my_str_to_bool(row, endpoint):
     for field in fields:
         row[field] = True if row[field] == "TRUE" else False
     return row
+
+
+def make_table(header, dataset):
+    table = ','.join(header) + '\n'
+    for row_data in dataset:
+        for field in header:
+            table += f"{row_data[field]},"
+        table += '\n'
+    return table
 
 
 def get_contest_id(state, current_chamber, contest_chambers):
@@ -120,10 +168,16 @@ def make_person_struct(dataset, chamber, contest_chambers, header):
     return people
 
 
-def make_table(header, dataset):
-    table = ','.join(header) + '\n'
-    for row_data in dataset:
-        for field in header:
-            table += f"{row_data[field]},"
-        table += '\n'
-    return table
+def make_other_names_struct(dataset):
+    row = dict()
+    result = []
+    for i, data in enumerate(dataset, start=1):
+        if data["nickname"]:
+            result.append({"other_name_type": 2,
+                           "name": data["nickname"],
+                           "person_id": i})
+    return result
+
+
+def make_person_profession(dataset, professions):
+    pass
