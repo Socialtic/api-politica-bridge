437b608293dc476b52b98f3d943bb5846b5c2597
Diego B <dbarriga@ciencias.unam.mx>
2021-05-11 00:56:56 -0500

    feature(static_tables): Writing csv files

--- static_tables.py
+++ static_tables.py
@@ -1,5 +1,5 @@
 from sheets import sheet_reader
-from utils import colors_to_list, make_banner
+from utils import colors_to_list, make_banner, make_table, write_csv
 
 SHEET_ID = "1fKXpwXhKlLLG-kjh8udQIH9poNLs7kAzSnXndZ1Le4Y"
 
@@ -14,27 +14,42 @@ ST_RANGES = {
 make_banner("Getting static tables data")
 # AREA
 area_data = sheet_reader(SHEET_ID, f"Table area!{ST_RANGES['area']}")
+area_header = area_data[0].keys()
+write_csv(make_table(area_header, area_data), "dataset/area")
 # CHAMBER
 chamber_data = sheet_reader(SHEET_ID, f"Table chamber!{ST_RANGES['chamber']}")
+chamber_header = chamber_data[0].keys()
+write_csv(make_table(chamber_header, chamber_data), "dataset/chamber")
 # ROLE
 role_data = sheet_reader(SHEET_ID, f"Table role!{ST_RANGES['role']}")
+role_header = role_data[0].keys()
+write_csv(make_table(role_header, role_data), "dataset/role")
 # COALITION
 coalition_data = sheet_reader(SHEET_ID,
                               f"Table coalition!{ST_RANGES['coalition']}")
+coalition_header = coalition_data[0].keys()
+write_csv(make_table(coalition_header, coalition_data), "dataset/coalition")
 coalition_data = colors_to_list(coalition_data)
 coalitions_catalogue = sheet_reader(SHEET_ID, "Table coalition!B2:B37",
                                     as_list=True)
 # PARTY
 party_data = sheet_reader(SHEET_ID, f"Table party!{ST_RANGES['party']}")
+party_header = party_data[0].keys()
+write_csv(make_table(party_header, party_data), "dataset/party")
 party_data = colors_to_list(party_data)
 parties = sheet_reader(SHEET_ID, "Table party!C2:C78", as_list=True)
 # CONTEST
 contest_data = sheet_reader(SHEET_ID, f"Table contest!{ST_RANGES['contest']}")
+contest_header = contest_data[0].keys()
+write_csv(make_table(contest_header, contest_data), "dataset/contest")
 contest_chambers = sheet_reader(SHEET_ID, "Table contest!C2:C358",
                                 as_list=True)
 # PROFESSION
 profession_range = f"Catalogue profession!{ST_RANGES['profession']}"
 profession_data = sheet_reader(SHEET_ID, profession_range)
+profession_header = profession_data[0].keys()
+write_csv(make_table(profession_header, profession_data), "dataset/profession")
 professions_catalogue = sheet_reader(SHEET_ID, "Catalogue profession!B2:B119",
                                      as_list=True)
+# URL types Catalogue
 url_types = sheet_reader(SHEET_ID, "Catalogue url_types!B2:B23", as_list=True)
