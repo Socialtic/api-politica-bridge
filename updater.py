8d740690ebf91d27db8362a48b662228aeabca92
Diego B <dbarriga@ciencias.unam.mx>
2021-05-12 11:52:40 -0500

    feature(local dev): add support for local tests

--- updater.py
+++ updater.py
@@ -4,26 +4,33 @@ from sheets import sheet_reader
 #from static_tables import (area_data, chamber_data, role_data, coalition_data,
 #                           coalitions_catalogue, party_data, parties,
 #                           contest_data, contest_chambers, profession_data)
-from utils import make_table, write_csv, make_banner, send_new_data
+from utils import make_table, write_csv, make_banner, send_new_data, read_csv, row_to_dict
 
 SHEET_ID = "1mk9LTI5RBYwrEPzILeDY925VJbLVmEoZyRzaa1gZ_hk"
 DATA_PATH = 'dataset/'
 API_BASE = "http://localhost:5000/"
 
+local = True
+
 def main():
-    dataset = sheet_reader(SHEET_ID, "TODO!A1:AF2423")
-    header = dataset[0].keys()
-    table = make_table(header, dataset)
-    write_csv(table, f"{DATA_PATH}current")
+    if local:
+        dataset = read_csv("dataset/current")
+        header = dataset.pop(0)
+        dataset = [row_to_dict(row, header) for row in dataset]
+    else:
+        dataset = sheet_reader(SHEET_ID, "TODO!A1:AF2423")
+        header = dataset[0].keys()
+        table = make_table(header, dataset)
+        write_csv(table, f"{DATA_PATH}current")
     if not os.path.isfile('dataset/old.csv'):
         write_csv(table, f"{DATA_PATH}/old")
         print("ALL IS UPDATED :)")
         return 0
+    breakpoint()
     diff = compare(
         load_csv(open(f'{DATA_PATH}old.csv'), key='person_id'),
         load_csv(open(f'{DATA_PATH}current.csv'), key='person_id'),
     )
-    breakpoint()
     if diff["changed"]:
         make_banner("There are changes :O")
         for change_data in diff["changed"]:
